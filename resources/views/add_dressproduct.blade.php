0577924f92172f203277d2d7c8e40c1343475bae
mukeshdeveloper <mukeshgeetha56@gmail.com>
2023-09-14 16:57:20 +0530

    1st commit

--- resources/views/add_dressproduct.blade.php
+++ resources/views/add_dressproduct.blade.php
@@ -234,7 +234,7 @@
                   document.write(new Date().getFullYear())
                 </script>,
                 made with <i class="fa fa-heart"></i> by
-                <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Mukesh</a>
+                <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Mukesh</a><button></button>
                 for a better web.
               </div>
             </div>
